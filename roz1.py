abe596e6dcca0ac9d94977c730c29a1d790307ca
lukasz15157 <szpot.l@pg.com>
2020-03-21 18:06:47 +0100

    my first commit

--- roz1.py
+++ roz1.py
@@ -2,3 +2,8 @@ print("witaj w moim programie")
 print("jak masz na imie")
 myname=input()
 print('witaj' + myname + '. Milo Cie widziec')
+
+if myname == 'lukasz'
+    print("witaj lukasz")
+else
+    print("witaj niezn
